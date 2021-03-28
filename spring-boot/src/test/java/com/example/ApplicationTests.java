7b5f926811c9a2dd5a89dc7dc27b01b5f89860a8
fjding <fjding@qq.com>
2021-03-28 18:31:49 +0800

    yue2add

--- spring-boot/src/test/java/com/example/ApplicationTests.java
+++ spring-boot/src/test/java/com/example/ApplicationTests.java
@@ -28,7 +28,7 @@ class ApplicationTests {
         System.out.println("bbb");
         System.out.println("3333");
         System.out.println("444");
-        System.out.println("555");
+        System.out.println("52add5");
     }
 
 
